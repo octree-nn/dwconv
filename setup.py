bd17f435ba84b5c141e7bcf8c7e02fbbdb81348d
wang-ps <wangps@hotmail.com>
2023-05-10 22:00:22 +0800

    Add README.md and LISCENCE

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+__version__ = '1.1.0'
+
+with open("README.md", "r", encoding="utf-8") as fid:
+  long_description = fid.read()
+
 
 dir = './csrc'
 sources = ['{}/{}'.format(dir, src) for src in os.listdir(dir)
@@ -8,11 +14,20 @@ sources = ['{}/{}'.format(dir, src) for src in os.listdir(dir)
 
 setup(
     name='dwconv',
-    version='1.0.0',
+    version=__version__,
+    author='Peng-Shuai Wang',
+    author_email='wangps@hotmail.com',
+    url='https://github.com/octree-nn/dwconv',
+    description='Octree-based Depth-Wise Convolution',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=['dwconv'],
-    install_requires=["torch"],
+    # packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    install_requires=['torch', 'numpy'],
+    python_requires='>=3.8',
+    license='MIT',
     ext_modules=[
         CUDAExtension(
             name='dwconv.core',
@@ -21,4 +36,7 @@ setup(
     cmdclass={
         'build_ext': BuildExtension
     },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+    ],
 )
